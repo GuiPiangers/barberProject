a8252c514ec20ecc6ba0bc3c72f986469d3f6642
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-05-31 18:57:24 -0300

    Add loading

--- src/Components/usual/Loading/Loading.jsx
+++ src/Components/usual/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import './Loading.css'
 export default function Loading(){
     return(
         <div className='loading'>
-            <img src="http://localhost:5173/loading.svg" alt="loading" />
+            <img src="http://localhost:5173/loading.gif" alt="loading" />
         </div>
     )
 }
