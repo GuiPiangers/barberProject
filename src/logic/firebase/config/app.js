9f4f8d1a2dc11eec2e92a10a3c7b12ce8279cf03
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-06-13 13:44:34 -0300

    change db

--- src/logic/firebase/config/app.js
+++ src/logic/firebase/config/app.js
@@ -1,18 +1,13 @@
 import { initializeApp } from "firebase/app"
 
 const app = initializeApp({
-    // apiKey: "AIzaSyCip44oOHu7fQ34DGoQeanW9AC7uiuoLKs",
-    // authDomain: "barber-project-e7916.firebaseapp.com",
-    // projectId: "barber-project-e7916",
-    // storageBucket: "barber-project-e7916.appspot.com",
-    // messagingSenderId: "729338516775",
-    // appId: "1:729338516775:web:9e8a52a7d282d1dd758502"
-    apiKey: "AIzaSyDQM5vt5xscUAe--rIH7_sLSouyFXOzTkY",
-    authDomain: "aula-bitcent.firebaseapp.com",
-    projectId: "aula-bitcent",
-    storageBucket: "aula-bitcent.appspot.com",
-    messagingSenderId: "93737230880",
-    appId: "1:93737230880:web:8dc9fb8657d7373b9b5245"
+
+    apiKey: "AIzaSyCip44oOHu7fQ34DGoQeanW9AC7uiuoLKs",
+    authDomain: "barber-project-e7916.firebaseapp.com",
+    projectId: "barber-project-e7916",
+    storageBucket: "barber-project-e7916.appspot.com",
+    messagingSenderId: "729338516775",
+    appId: "1:729338516775:web:9e8a52a7d282d1dd758502"
 })
 
 export {app}
