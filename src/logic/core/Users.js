a9f276908df8e709a3788285e425170d96159c9d
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-06-13 14:40:30 -0300

    change default user img

--- src/logic/core/Users.js
+++ src/logic/core/Users.js
@@ -33,7 +33,7 @@ export default class Users {
     async createUserEmailPassword(email, password, name){
         const user = await this._authentication.createUserEmailPassword(email, password)
         if (!user) return null
-        const newUser = {...user, name: name, image: 'http://localhost:5173/img/default-user.png'}
+        const newUser = {...user, name: name, image: 'https://firebasestorage.googleapis.com/v0/b/barber-project-e7916.appspot.com/o/default-user.png?alt=media&token=a15d4522-5c0c-4d78-a3b1-4eaa3dab4602&_gl=1*1yf0o9w*_ga*MTY4OTQ5NDYwMi4xNjgzODM2NTc1*_ga_CW55HF8NVT*MTY4NjY3MzcxMi41Ni4xLjE2ODY2Nzc0OTYuMC4wLjA.'}
 
         let userData = await this.search(user.id)
         if (!userData) userData = await this.set(newUser)
