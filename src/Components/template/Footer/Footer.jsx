7185c82818186561d3f7e07e3b2710e753619998
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-06-19 18:52:22 -0300

    add icons to link

--- src/Components/template/Footer/Footer.jsx
+++ src/Components/template/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import './Footer.css'
+import { FaWhatsapp, FaLinkedin } from 'react-icons/fa'
+import { SiGmail } from 'react-icons/si'
 import BoxContainer from '../BoxContainer/BoxContainer'
 import FooterLink from './FooterLink'
 
@@ -15,9 +17,9 @@ export default function Footer(){
                         
                         <h2 className='footer__title'>Contato</h2>
                         
-                        <FooterLink url='https://api.whatsapp.com/send/?phone=5551980351927'>(51) 9 8035-1927</FooterLink>
-                        <FooterLink url='https://www.linkedin.com/in/guilherme-piangers'>guilherme-piangers</FooterLink>
-                        <FooterLink url='mailto:piangersguilherme@gmail.com'>piangersguilherme@gmail.com</FooterLink>
+                        <FooterLink url='https://api.whatsapp.com/send/?phone=5551980351927'><FaWhatsapp/> (51) 9 8035-1927</FooterLink>
+                        <FooterLink url='https://www.linkedin.com/in/guilherme-piangers'><FaLinkedin/> guilherme-piangers</FooterLink>
+                        <FooterLink url='mailto:piangersguilherme@gmail.com'><SiGmail/> piangersguilherme@gmail.com</FooterLink>
                     </div>
                 </BoxContainer>
 
