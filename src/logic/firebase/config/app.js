1852a6dbe02db507d8bf10e2ba45786721f440d7
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2023-06-20 22:03:30 -0300

    remove coments

--- src/logic/firebase/config/app.js
+++ src/logic/firebase/config/app.js
@@ -15,13 +15,6 @@ const app = initializeApp({
     storageBucket: storageBucket,
     messagingSenderId: messagingSenderId,
     appId: appId
-
-    // apiKey: "AIzaSyCip44oOHu7fQ34DGoQeanW9AC7uiuoLKs",
-    // authDomain: "barber-project-e7916.firebaseapp.com",
-    // projectId: "barber-project-e7916",
-    // storageBucket: "barber-project-e7916.appspot.com",
-    // messagingSenderId: "729338516775",
-    // appId: "1:729338516775:web:9e8a52a7d282d1dd758502"
 })
 
 export {app}
